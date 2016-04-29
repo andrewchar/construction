df1d89c1cefea5a0fc8c5478eff45e6e469eced0
andrew charlebois <andrew.charlebois@hotmail.com>
2016-04-28 22:52:53 -0400

    final push, v1.0.0

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,13 +1,28 @@
-$(document).ready(function(){
-  //CAROUSEL
-  $('.fade').slick({
-  // infinite: true,  TURN OFF WHILE WORKING
-  speed: 500,
-  fade: true,
-  cssEase: 'linear',
-  // autoplay: true,   TURN OFF WHILE WORKING
-  autoplaySpeed: 5000,
-  pauseOnHover: false
+// SMOOTH SCROLL
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 300);
+        return false;
+      }
+    }
+  });
 });
+
+$(document).ready(function(){
+
+  jQuery('.pictures-demo').slippry( {
+    captions: false,
+    responsive: true,
+    pager: false,
+    controls: false,
+    speed: 4000,
+    autoHover: false
+  });
 
 });
